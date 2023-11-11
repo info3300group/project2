da16c7fad2b3376cbd0381530c847a7012efcfde
Morgan <mrgnsuh@gmail.com>
2023-11-11 14:20:46 -0500

    median prices

---
+++ find_median_price.py
@@ -0,0 +1,26 @@
+import os
+import pandas as pd
+import json
+
+root_directory = 'NYCPropertyCosts'
+boroughs = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'StatenIsland']
+years = list(range(2010, 2018))
+
+median_prices_dict = {}
+
+for borough in boroughs:
+    median_prices_dict[borough] = {}
+    for year in years:
+        file_path = os.path.join(root_directory, borough, f'{year}_{borough}.csv')
+
+        if os.path.exists(file_path):
+            df = pd.read_csv(file_path)
+
+            grouped_data = df[df['Zip Code'] != 0].groupby('Zip Code')['Sale Price'].median().reset_index()
+            median_prices_dict[borough][str(year)] = grouped_data.to_dict(orient='records')
+
+# Save the dictionary to a JSON file
+with open('median_prices_by_zip.json', 'w') as json_file:
+    json.dump(median_prices_dict, json_file, indent=2)
+
+print('Median prices by zip code saved to median_prices_by_zip.json')
